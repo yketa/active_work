a2e934e25d5f5a7e5d53c64f1e07b9e84cce6f5d
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2019-10-14 22:20:39 +0100

    commented parameters

--- main.cpp
+++ main.cpp
@@ -11,25 +11,26 @@ int main() {
   // VARIABLE DEFINITION
 
   // system
-  double N = getEnvDouble("N", 1);
-  double lp = getEnvDouble("LP", 1);
-  double phi = getEnvDouble("PHI", 0.02);
-  int seed = getEnvInt("SEED", 1);
-  std::string filename = getEnvString("FILE", "out.dat");
+  double N = getEnvDouble("N", 1); // number of particles in the system
+  double lp = getEnvDouble("LP", 1); // dimensionless persistence length
+  double phi = getEnvDouble("PHI", 0.02); // packing fraction
+  int seed = getEnvInt("SEED", 1); // random seed
+  std::string filename = getEnvString("FILE", "out.dat"); // output file name
 
   // simulation
-  double dt = getEnvDouble("DT", 1);
-  int Niter = getEnvInt("NITER", 1000000);
+  double dt = getEnvDouble("DT", 1); // time step
+  int Niter = getEnvInt("NITER", 1000000); // number of iterations
+
+  // active work computation
+  int nWork = getEnvInt("NWORK", 0); // number of frames on which to compute active work
 
   // SYSTEM
 
-  System system(N, lp, phi, seed, dt, filename);
-  system.saveInitialState();
+  System system(N, lp, phi, seed, dt, filename, nWork); // custom number of frames for active work computation
+  system.saveInitialState(); // save first frame
 
   // ITERATION
 
-  for (int i=0; i < Niter; i++) {
-    iterate_ABP_WCA(&system);
-  }
+  for (int i=0; i < Niter; i++) { iterate_ABP_WCA(&system); }
 
 }
