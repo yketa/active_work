b8bf3b1eb1757cec0a50fe121b169e8c68e6d8b8
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2019-10-15 15:04:44 +0100

    getEnvBool

--- env.cpp
+++ env.cpp
@@ -30,6 +30,15 @@ int getEnvInt(std::string const& envName, int const& defaultValue) {
   }
 }
 
+bool getEnvBool(std::string const& envName, bool const& defaultValue) {
+  // Returns environment variable as boolean.
+  // WARNING: Be EXTRA CAREFUL with this function, only use "0" and "1" as
+  // environment variables.
+  // (from https://stackoverflow.com/questions/5866134/how-to-read-linux-environment-variables-in-c)
+
+  return (bool) getEnvInt(envName, defaultValue);
+}
+
 std::string getEnvString(std::string const& envName,
   std::string const& defaultValue) {
   // Returns environment variable as string.
--- env.h
+++ env.h
@@ -13,6 +13,9 @@ double getEnvDouble(std::string const& envName, double const& defaultValue = 0);
 int getEnvInt(std::string const& envName, int const& defaultValue = 0);
   // Returns environment variable as integer.
 
+bool getEnvBool(std::string const& envName, bool const& defaultValue = 0);
+  // Returns environment variable as boolean.
+
 std::string getEnvString(std::string const& envName,
   std::string const& defaultValue = "");
   // Returns environment variable as string.
