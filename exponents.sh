5130c7f649176e28ef21002ef76af6d7905a9ea7
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2019-11-18 11:43:49 +0000

    bash commands for exponents.py

---
+++ exponents.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# This bash shell script enables one to use functions
+# active_work.exponents.float_to_letters and
+# active_work.exponents.letters_to_float from the shell.
+#
+# Information about litteral notation of floats used in this project can be
+# found in active_work/exponents.py.
+
+letters_to_float(){
+	# Converts litteral expression to float expression.
+	/usr/bin/python3 -c "from active_work.exponents import letters_to_float; print(letters_to_float('$1'))"
+}
+export -f letters_to_float
+
+float_to_letters(){
+	# Converts float expression to litteral expression.
+	/usr/bin/python3 -c "from active_work.exponents import float_to_letters; print(float_to_letters($1))"
+}
+export -f float_to_letters
