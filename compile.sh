53775211c8aa0f954e28d5493b20d3250f558639
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2019-12-28 13:12:01 +0100

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Compile all executables.
+
+for CL in yes no; do  # with and without cell lists
+  # SIMULATIONS
+  for S0 in yes no; do  # general ABPs and custom model
+    make clean && SIM0=$S0 CELLLIST=$CL make;
+  done
+  # CLONING
+  for CD in {0..3}; do  # different controlled dynamics
+    make clean && CLONING=yes CONTROLLED_DYNAMICS=$CD CELLLIST=$CL make;
+  done
+done
